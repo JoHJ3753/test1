e5d09cd1fc6c56be623dc82e886fac0c7d5746a6
JoHJ3753 <guswns3753@gmail.com>
2021-07-01 09:50:31 +0900

    5th commit
    다섯번째 커밋

--- board/src/main/java/com/board/dao/BoardDAOImpl.java
+++ board/src/main/java/com/board/dao/BoardDAOImpl.java
@@ -2,6 +2,8 @@ package com.board.dao;
 
 import java.util.List;
 
+import java.util.HashMap;
+
 import javax.inject.Inject;
 
 import org.apache.ibatis.session.SqlSession;
@@ -39,5 +41,25 @@ public class BoardDAOImpl implements BoardDAO {
  public void modify(BoardVO vo) throws Exception {
  sql.update(namespace + ".modify", vo);
 }
- 
+ //게시물 삭제
+ public void delete(int bno) throws Exception {
+ sql.delete(namespace + ".delete", bno);
+}
+ //게시물 총 갯수
+ @Override
+ public int count() throws Exception {
+	return sql.selectOne(namespace + ".count"); 
+}
+ //게시물 목록 + 페이징
+ @Override
+ public List listPage(int displayPost, int postNum) throws Exception {
+
+	HashMap<String, Integer> data = new HashMap<String, Integer>();
+
+	data.put("displayPost", displayPost);
+	data.put("postNum", postNum);
+
+	return sql.selectList(namespace + ".listPage", data);
+}
+
 }
